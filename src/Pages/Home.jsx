a10cd013d0443caf50cd76416a04320c5662f448
swarup-1 <swarupkadoli9@gmail.com>
2022-12-16 16:24:17 +0530

    day-4

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -44,50 +44,122 @@ function Home() {
     {
       heading:"Latest News & Videos",
       otherLinks:{
-        li1:"Save Lives with Your Blog",
+        li1:"Save Lives with Your Blog ",
         li2:"•   News & Articles   •",
-        li3:"Follow Our Facebook"
+        li3:" Follow Our Facebook"
       },
-      elements:{
-        images:{
-          img1:"https://scontent-xsp1-2.xx.fbcdn.net/v/t15.5256-10/127853511_500056460971216_6625358320206453732_n.jpg?stp=dst-jpg_s720x720&_nc_cat=103&ccb=1-7&_nc_sid=ad6a45&_nc_ohc=cT8flw6e3ssAX97i3UO&_nc_ht=scontent-xsp1-2.xx&edm=ALdPpPkEAAAA&oh=00_AfDHLN1Dz1I2SoISj0ernr-9FCagKvkvyh0iRHlflWe-XA&oe=639F7B11",
-          img2:"https://scontent-xsp1-2.xx.fbcdn.net/v/t15.5256-10/127853511_500056460971216_6625358320206453732_n.jpg?stp=dst-jpg_s720x720&_nc_cat=103&ccb=1-7&_nc_sid=ad6a45&_nc_ohc=cT8flw6e3ssAX97i3UO&_nc_ht=scontent-xsp1-2.xx&edm=ALdPpPkEAAAA&oh=00_AfDHLN1Dz1I2SoISj0ernr-9FCagKvkvyh0iRHlflWe-XA&oe=639F7B11",
-          img3:"https://scontent-xsp1-2.xx.fbcdn.net/v/t15.5256-10/127853511_500056460971216_6625358320206453732_n.jpg?stp=dst-jpg_s720x720&_nc_cat=103&ccb=1-7&_nc_sid=ad6a45&_nc_ohc=cT8flw6e3ssAX97i3UO&_nc_ht=scontent-xsp1-2.xx&edm=ALdPpPkEAAAA&oh=00_AfDHLN1Dz1I2SoISj0ernr-9FCagKvkvyh0iRHlflWe-XA&oe=639F7B11",
-          img4:"https://scontent-xsp1-2.xx.fbcdn.net/v/t15.5256-10/127853511_500056460971216_6625358320206453732_n.jpg?stp=dst-jpg_s720x720&_nc_cat=103&ccb=1-7&_nc_sid=ad6a45&_nc_ohc=cT8flw6e3ssAX97i3UO&_nc_ht=scontent-xsp1-2.xx&edm=ALdPpPkEAAAA&oh=00_AfDHLN1Dz1I2SoISj0ernr-9FCagKvkvyh0iRHlflWe-XA&oe=639F7B11",
-          img5:"https://scontent-xsp1-2.xx.fbcdn.net/v/t15.5256-10/127853511_500056460971216_6625358320206453732_n.jpg?stp=dst-jpg_s720x720&_nc_cat=103&ccb=1-7&_nc_sid=ad6a45&_nc_ohc=cT8flw6e3ssAX97i3UO&_nc_ht=scontent-xsp1-2.xx&edm=ALdPpPkEAAAA&oh=00_AfDHLN1Dz1I2SoISj0ernr-9FCagKvkvyh0iRHlflWe-XA&oe=639F7B11"
+      elements:[
+        {
+          img:"https://scontent-xsp1-2.xx.fbcdn.net/v/t15.5256-10/127853511_500056460971216_6625358320206453732_n.jpg?stp=dst-jpg_s720x720&_nc_cat=103&ccb=1-7&_nc_sid=ad6a45&_nc_ohc=cT8flw6e3ssAX97i3UO&_nc_ht=scontent-xsp1-2.xx&edm=ALdPpPkEAAAA&oh=00_AfDHLN1Dz1I2SoISj0ernr-9FCagKvkvyh0iRHlflWe-XA&oe=639F7B11",
+          link:"Salted Caramegggggggggl Apple Crumble!"
         },
-        links:{
-          link1:"Salted Caramel Apple Crumble!",
-          link2:"Salted Caramel Apple Crumble!",
-          link3:"Salted Caramel Apple Crumble!",
-          link4:"Salted Caramel Apple Crumble!",
-          link5:"Salted Caramel Apple Crumble!",
+        {
+          img:"https://scontent-xsp1-2.xx.fbcdn.net/v/t15.5256-10/313572633_1222446185289163_301818545381834372_n.jpg?stp=dst-jpg_s720x720&_nc_cat=1&ccb=1-7&_nc_sid=ad6a45&_nc_ohc=YQIGmo8tlTkAX9uSzj1&_nc_ht=scontent-xsp1-2.xx&edm=ALdPpPkEAAAA&oh=00_AfCAmO-LxRCjXSY1rE0M3ByCqX9lC9fls7rd_4CLHcIC5w&oe=63A19575",
+          link:"Wild Turkeys gggggggggGo Nuts About!",
+        },
+        {
+          img:"https://scontent-xsp1-2.xx.fbcdn.net/v/t15.5256-10/317917398_835831947653314_7294252524021926878_n.jpg?stp=dst-jpg_s720x720&_nc_cat=105&ccb=1-7&_nc_sid=ad6a45&_nc_ohc=hsHMEsxpCYYAX8jh38A&_nc_ht=scontent-xsp1-2.xx&edm=ALdPpPkEAAAA&oh=00_AfDkd2PRB6BOyoROnAZwU9nxWFY9QrfAZNo5iWlu_4wJHg&oe=63A1D530",
+          link:"Sick Baby Cowgggggggggsf Headbutts",
+        },
+        {
+          img:"https://scontent-xsp1-2.xx.fbcdn.net/v/t15.5256-10/318915884_561186642153164_127441678623272126_n.jpg?stp=dst-jpg_s720x720&_nc_cat=103&ccb=1-7&_nc_sid=ad6a45&_nc_ohc=RbU2RzfjjLkAX9GaRhy&_nc_ht=scontent-xsp1-2.xx&edm=ALdPpPkEAAAA&oh=00_AfB_yJsQegCoLtqzEXYf5cr9wdpKOPZ26zW3oij8Gj720A&oe=63A1215C",
+          link:"The Best Air Fryer Recipe",
+        },
+        {
+          img:"https://scontent-xsp1-2.xx.fbcdn.net/v/t15.5256-10/283214530_476021257630109_5388539080951558766_n.jpg?stp=dst-jpg_p720x720&_nc_cat=104&ccb=1-7&_nc_sid=ad6a45&_nc_ohc=xEbgWl_cqR8AX8oGPxP&_nc_ht=scontent-xsp1-2.xx&edm=ALdPpPkEAAAA&oh=00_AfAU_h8g1E1spqVverxvxQPIbovldQCmPxJ6Zz0CI8BHyA&oe=63A0854B",
+          link:"Sea Turtl At Just The Right Time",
         }
-      }
+      ],
     },
     {
       heading:"Yummylicious Moments",
-      otherLinks:{},
-      elements:{
-        images:{},
-        links:{}
-      }
+      otherLinks:{
+        li1:"Secret Recipes ",
+        li2:"•   Member Hot Picks   •",
+        li3:" Photo Moments"
+      },
+      elements:[
+        {
+          img:"https://www.kindmeal.my/photos/moment/24/24754-48161-s.jpg",
+          link:"Purple Cane Tea Restaurant @ The Gardens - VyVian!"
+        },
+        {
+          img:"https://www.kindmeal.my/photos/moment/24/24753-48157-s.jpg",
+          link:"Pine 3 Vegetarian Restaurent - VyVian!",
+        },
+        {
+          img:"https://www.kindmeal.my/photos/moment/24/24752-48154-s.jpg",
+          link:"Pure Heart Vegetarian - William21",
+        },
+        {
+          img:"https://www.kindmeal.my/photos/moment/24/24752-48154-s.jpg",
+          link:"MyTOWN Shopping Centre - FatJedi",
+        },
+        {
+          img:"https://www.kindmeal.my/photos/member/42/42102-m.jpg",
+          link:"Paharganj - sonalivarma",
+        }
+      ],
     },
     {
       heading:"Discover Restaurants",
-      otherLinks:{},
-      elements:{
-        images:{},
-        links:{}
-      }
+      otherLinks:{
+        li1:"Vegetarian Directory ",
+        li2:"• Restaurant Menu •",
+        li3:" Food Map"
+      },
+      elements:[
+        {
+          img:"https://www.kindmeal.my/photos/item/0/292-3016-s.jpg",
+          link:"BOOSTER - carrot, beetroot and orange!"
+        },
+        {
+          img:"https://www.kindmeal.my/photos/item/0/331-3451-s.jpg",
+          link:"Al Funghi Pasta!",
+        },
+        {
+          img:"https://www.kindmeal.my/photos/item/0/418-5750-s.jpg",
+          link:"Affogato",
+        },
+        {
+          img:"https://www.kindmeal.my/photos/item/0/627-9654-s.jpg",
+          link:"Ice Grey Latte",
+        },
+        {
+          img:"https://www.kindmeal.my/photos/item/0/350-3728-s.jpg",
+          link:"Pesto Pasta",
+        }
+      ],
     },
     {
       heading:"Amazing Superheroes",
-      otherLinks:{},
-      elements:{
-        images:{},
-        links:{}
-      }
+      otherLinks:{
+        li1:"Latest Buzz ",
+        li2:"•    Lifestyle Ambassadors    •",
+        li3:" Win Gifts!"
+      },
+      elements:[
+        {
+          img:"https://www.kindmeal.my/photos/member/41/41364-m.jpg",
+          link:"DebbieNg1"
+        },
+        {
+          img:"https://www.kindmeal.my/photos/member/42/42102-m.jpg",
+          link:"TowhidulIslam!",
+        },
+        {
+          img:"https://www.kindmeal.my/photos/member/19/19852-m.jpg",
+          link:"JennyOoi",
+        },
+        {
+          img:"https://www.kindmeal.my/photos/member/35/35327-m.jpg",
+          link:"KehChinChoo",
+        },
+        {
+          img:"https://www.kindmeal.my/photos/member/19/19739-m.jpg",
+          link:"KarenKhoo",
+        }
+      ],
     },
   ]
   return (
@@ -117,27 +189,13 @@ function Home() {
                 <Link>{e.otherLinks.li3}</Link>
               </Box>
             </Flex>
-            <Flex justify="space-between">
-              <Box>
-                <Image src={e.elements.images.img1} h="200px" w="200px" objectFit='cover' borderRadius="5px"/>
-                <Link>{e.elements.links.link1}</Link>
-              </Box>
-              <Box>
-                <Image src={e.elements.images.img2} h="200px" w="200px" objectFit='cover' borderRadius="5px"/>
-                <Link>{e.elements.links.link2}</Link>
-              </Box>
-              <Box>
-                <Image src={e.elements.images.img3} h="200px" w="200px" objectFit='cover' borderRadius="5px"/>
-                <Link>{e.elements.links.link3}</Link>
-              </Box>
-              <Box>
-                <Image src={e.elements.images.img4} h="200px" w="200px" objectFit='cover' borderRadius="5px"/>
-                <Link>{e.elements.links.link4}</Link>
-              </Box>
-              <Box>
-                <Image src={e.elements.images.img5} h="200px" w="200px" objectFit='cover' borderRadius="5px"/>
-                <Link>{e.elements.links.link5}</Link>
-              </Box>
+            <Flex justifyContent="space-around" gap="20px" flexDirection="row">
+              {e.elements && e.elements.map((el)=>
+                  <Box w="200px">
+                    <Image src={el.img} h="200px" w="100%" objectFit='cover' borderRadius="5px"/>
+                    <Link textAlign="justify" fontSize="12px" >{el.link}</Link>
+                  </Box>
+              )}
             </Flex>
           </Flex>
         </Center>
@@ -147,3 +205,33 @@ function Home() {
 }
 
 export default Home
+
+
+
+
+
+
+
+
+
+
+              {/* <Box w="200px">
+                <Image src={e.elements.images.img1} h="200px" w="100%" objectFit='cover' borderRadius="5px"/>
+                <Link textAlign="justify" fontSize="12px" >{e.elements.links.link1}</Link>
+              </Box>
+              <Box w="200px">
+                <Image src={e.elements.images.img2} h="200px" w="100%" objectFit='cover' borderRadius="5px"/>
+                <Link textAlign="justify" fontSize="12px" >{e.elements.links.link2}</Link>
+              </Box>
+              <Box w="200px">
+                <Image src={e.elements.images.img3} h="200px" w="100%" objectFit='cover' borderRadius="5px"/>
+                <Link textAlign="justify" fontSize="12px" >{e.elements.links.link3}</Link>
+              </Box>
+              <Box w="200px">
+                <Image src={e.elements.images.img4} h="200px" w="100%" objectFit='cover' borderRadius="5px"/>
+                <Link textAlign="justify" fontSize="12px" >{e.elements.links.link4}</Link>
+              </Box>
+              <Box w="200px">
+                <Image src={e.elements.images.img5} h="200px" w="100%" objectFit='cover' borderRadius="5px"/>
+                <Link textAlign="justify" fontSize="12px" >{e.elements.links.link5}</Link>
+              </Box> */}
